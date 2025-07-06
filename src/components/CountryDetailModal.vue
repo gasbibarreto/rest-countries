8bb47dc8de54e27cc2cecf8b0f7ea3570fdc21c5
Gabriella Barreto <gabriellabarretogomes@gmail.com>
2025-07-06 11:25:30 -0300

    ajustando o country modal

--- src/components/CountryDetailModal.vue
+++ src/components/CountryDetailModal.vue
@@ -3,51 +3,56 @@
 		<button @click="closeModal" class="country-detail__back-button">
 			← Back
 		</button>
-		<div v-if="country">
+		<div v-if="country" class="country-detail__body">
 			<img
-				:src="country.flags.png"
+				:src="country.flags.svg"
 				:alt="`Bandeira de ${country.name.common}`"
 				class="country-detail__flag" />
-			<h2 class="country-detail__name">{{ country.name.common }}</h2>
-			<div class="country-detail__info">
-				<p><strong>Native Name:</strong> {{ country.name.official }}</p>
-				<p>
-					<strong>Population:</strong> {{ country.population.toLocaleString() }}
-				</p>
-				<p><strong>Region:</strong> {{ country.region }}</p>
-				<p><strong>Sub Region:</strong> {{ country.subregion }}</p>
-				<p>
-					<strong>Capital:</strong>
-					{{
-						country.capital && country.capital.length > 0
-							? country.capital.join(', ')
-							: 'N/A'
-					}}
-				</p>
-				<p>
-					<strong>Top Level Domain:</strong>
-					{{ country.tld && country.tld.join(', ') }}
-				</p>
-				<p>
-					<strong>Currencies:</strong>
-					{{
-						Object.values(country.currencies || {})
-							.map((c) => c.name)
-							.join(', ')
-					}}
-				</p>
-				<p>
-					<strong>Languages:</strong>
-					{{ Object.values(country.languages || {}).join(', ') }}
-				</p>
-			</div>
-			<div class="country-detail__borders">
-				<h3 class="country-detail__borders-title">Border Countries:</h3>
-				<div class="country-detail__borders-list">
-					<span v-if="country.borders && country.borders.length > 0">
-						{{ country.borders.join(', ') }}
-					</span>
-					<span v-else>Nenhuma fronteira terrestre</span>
+			<div class="country-detail__content">
+				<h2 class="country-detail__name">{{ country.name.common }}</h2>
+				<div class="country-detail__info">
+					<p><strong>Native Name:</strong> {{ country.name.official }}</p>
+					<p>
+						<strong>Population:</strong>
+						{{ country.population.toLocaleString() }}
+					</p>
+					<p><strong>Region:</strong> {{ country.region }}</p>
+					<p><strong>Sub Region:</strong> {{ country.subregion }}</p>
+					<p>
+						<strong>Capital:</strong>
+						{{
+							country.capital && country.capital.length > 0
+								? country.capital.join(', ')
+								: 'N/A'
+						}}
+					</p>
+					<p>
+						<strong>Top Level Domain:</strong>
+						{{ country.tld && country.tld.join(', ') }}
+					</p>
+					<p>
+						<strong>Currencies:</strong>
+						{{
+							Object.values(country.currencies || {})
+								.map((c) => c.name)
+								.join(', ')
+						}}
+					</p>
+					<p>
+						<strong>Languages:</strong>
+						{{ Object.values(country.languages || {}).join(', ') }}
+					</p>
+				</div>
+				<div class="country-detail__borders">
+					<h3 class="country-detail__borders-title">Border Countries:</h3>
+					<div class="country-detail__borders-list">
+						<template v-if="country.borders && country.borders.length > 0">
+							<span v-for="border in country.borders" :key="border" class="country-detail__border">
+								{{ border }}
+							</span>
+						</template>
+						<span v-else>Nenhuma fronteira terrestre</span>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -143,8 +148,17 @@ const closeModal = () => {
 .country-detail__borders-list {
 	display: flex;
 	flex-wrap: wrap;
+}
+
+.country-detail__borders-list span {
+	padding: 8px 16px;
+	border-radius: 6px;
+	box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+	background-color: var(--card-color);
+	font-size: 14px;
 	gap: 12px;
 }
+
 
 .country-detail__border {
 	background-color: var(--button-bg);
@@ -154,8 +168,53 @@ const closeModal = () => {
 	box-shadow: 0 1px 4px var(--button-shadow);
 	font-size: 14px;
 }
+
 .country-detail.dark-theme {
 	background-color: var(--bg-color);
 	color: var(--text-color);
+}
+
+@media (min-width: 600px) {
+	.country-detail {
+		padding: 48px;
+	}
+
+	.country-detail__back-button {
+		font-size: 18px;
+	}
+
+	.country-detail__name {
+		font-size: 32px;
+	}
+
+	.country-detail__info p {
+		font-size: 16px;
+	}
+
+	.country-detail__borders-title {
+		font-size: 18px;
+	}
+
+	.country-detail__body {
+		display: flex;
+		align-items: center;
+		gap: 60px;
+	}
+
+	.country-detail__flag {
+		flex: 1;
+		max-width: 700px; /* Limita a largura da bandeira */
+		margin-bottom: 0;
+	}
+
+	.country-detail__content {
+		flex: 1;
+	}
+
+	.country-detail__info {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		gap: 10px 20px; /* Espaçamento entre linhas e colunas */
+	}
 }
 </style>
